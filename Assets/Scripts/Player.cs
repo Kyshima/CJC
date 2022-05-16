541e737b3066d39b44294a51073391c592305cf6
Patricia <39797@ufp.edu.pt>
2022-05-16 23:58:44 +0100

    Animaçoes

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class Player : Mover
 {
     private SpriteRenderer SpriteRenderer;
+    private Animator animator;
 
     protected override void Start()
     {
         base.Start();
         SpriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
     private void FixedUpdate()
     {
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
+
+        if(x!=0 || y!=0) animator.SetTrigger("Run");
+        else animator.ResetTrigger("Run");
 
         UpdateMotor(new Vector3(x,y,0));
     }

c3e11d59c3a865232b91e133b2edbf55db5b6f26
rpcmoreira <rpcardoso2@outlook.com>
2022-05-17 23:37:23 +0100

    Menu Death e Respawn

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -17,33 +17,35 @@ public class Camera : MonoBehaviour
     {
         Vector3 delta = Vector3.zero;
 
-        float deltaX = lookAt.position.x - transform.position.x;
-        if(deltaX > boundX || deltaX < -boundX)
+        if (lookAt != null)
         {
-            if(transform.position.x < lookAt.position.x)
+            float deltaX = lookAt.position.x - transform.position.x;
+            if (deltaX > boundX || deltaX < -boundX)
             {
-                delta.x = deltaX - boundX;
+                if (transform.position.x < lookAt.position.x)
+                {
+                    delta.x = deltaX - boundX;
+                }
+                else
+                {
+                    delta.x = deltaX + boundX;
+                }
             }
-            else
+
+            float deltaY = lookAt.position.y - transform.position.y;
+            if (deltaY > boundY || deltaY < -boundY)
             {
-                delta.x = deltaX + boundX;
+                if (transform.position.y < lookAt.position.y)
+                {
+                    delta.y = deltaY - boundY;
+                }
+                else
+                {
+                    delta.y = deltaY + boundY;
+                }
             }
+
+            transform.position += new Vector3(delta.x, delta.y, 0);
         }
-
-        float deltaY = lookAt.position.y - transform.position.y;
-        if(deltaY > boundY || deltaY < -boundY)
-        {
-            if(transform.position.y < lookAt.position.y)
-            {
-                delta.y = deltaY - boundY;
-            }
-            else
-            {
-                delta.y = deltaY + boundY;
-            }
-        }
-
-        transform.position += new Vector3(delta.x,delta.y,0);
-
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,7 @@ public class Player : Mover
 {
     private SpriteRenderer SpriteRenderer;
     private Animator animator;
+    private bool isAlive = true;
 
     protected override void Start()
     {
@@ -14,6 +15,20 @@ public class Player : Mover
         SpriteRenderer = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
     }
+
+    protected override void ReceiveDamage(Damage dmg)
+    {
+        if (!isAlive) return;
+        base.ReceiveDamage(dmg);
+    }
+
+    protected override void Death()
+    {
+        isAlive = false;
+        Destroy(gameObject);
+        GameManager.instance.deathAnim.SetTrigger("Dead");
+    }
+
     private void FixedUpdate()
     {
         float x = Input.GetAxisRaw("Horizontal");
@@ -22,6 +37,14 @@ public class Player : Mover
         if(x!=0 || y!=0) animator.SetTrigger("Run");
         else animator.ResetTrigger("Run");
 
-        UpdateMotor(new Vector3(x,y,0));
+        if(isAlive) UpdateMotor(new Vector3(x,y,0));
     }
+
+    public void Respawn()
+    {
+        this.hp = 10;
+        isAlive = true;
+    }
+
+    
 }

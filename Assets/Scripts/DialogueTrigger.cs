90afcc71703c583f8dfa6a9233d95ac5d20e0a27
rpcmoreira <rpcardoso2@outlook.com>
2022-05-19 01:19:58 +0100

    Update DialogueTrigger.cs

--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -2,20 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DialogueTrigger : MonoBehaviour
+public class DialogueTrigger : Collidable
 {
     public Dialogue dialogue;
-
-    public void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.F))
-        {
-            TriggerDialogue();
-        }
-    }
 
     public void TriggerDialogue()
     {
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
     }
+
+    protected override void OnCollide(Collider2D col)
+    {
+        if (Input.GetKeyDown(KeyCode.F) && col.name == "PlayerCollider")
+        {
+            TriggerDialogue();
+        }
+    }
+
 }
